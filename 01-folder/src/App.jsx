8b3fecfddda69f36600ffe6afb3fa295004b75c9
Aparna <aparnayadav739@gmail.com>
2026-06-11 00:08:01 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id ='dad'>
+      <h1 id = 'child1'>hello</h1>
+      <h2 id ='child2'>hello</h2>
+      
+    </div>
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
